359a925f4f57df3f38c968842f93e8d7c9f89174
Your Name <you@example.com>
2022-08-03 19:32:04 +0200

    update with  likes and dislikes in DDBB

---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+const newUserController = async ( req, res, next) => {
+
+}
+const getUserController = async ( req, res, next) => {
+
+}
+const loginController = async ( req, res, next) => {
+
+}
+
+module.exports = { // cogemos este objeto  y lo copiamos en server.js que es donde lo vamos a importar.
+    newUserController,
+    getUserController,
+    loginController
+};
