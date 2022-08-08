879a273ad94d139c2636d80af4ae015d4a4d0995
Your Name <you@example.com>
2022-08-08 20:48:19 +0200

    vote files created

--- DDBB/votesdb.js
+++ DDBB/votesdb.js
@@ -1,27 +1,40 @@
+const { getConnection } = require('../DDBB/db');
+const { generateError } = require('../helpers');
 
-const { getConnection } = require ("../DDBB/db");
+const newVote = async (user_id, comment_id, vote) => {
+  let connection;
 
+  try {
+    connection = await getConnection();
+    // imprescindible comprobar que el usuario no haya votado ya ese comentario.
+    const [formervote] = await connection.query(
+      `
+      SELECT id FROM votes WHERE user_id = ? AND comment_id = ?
+      `,
+      [user_id, comment_id]
+    );
 
-
-
-const newVote = async ( vote,user_id, comment_id) => {
-    let connection;
-
-    try {
-        connection = await getConnection ();
-
-        const [result] = await connection.query (`
-        INSERT INTO votes (user_id, vote,comment_id)
-        VALUES (? ? ?)
-        `, [user_id, vote, comment_id]);
-        
-        return result.insertVote;
-
-        } finally {
-        if (connection) connection.release ();
+    if (formervote.length > 0) {
+      throw generateError(
+        `el usuario ${user_id} ya ha votado el comentario ${comment_id}`,
+        409
+      );
     }
-}; 
+
+    const [result] = await connection.query(
+      `
+        INSERT INTO votes (user_id, vote,comment_id)
+        VALUES (?, ?, ?)
+        `,
+      [user_id, vote, comment_id]
+    );
+
+    return result.insertId;
+  } finally {
+    if (connection) connection.release();
+  }
+};
 
 module.exports = {
-newVote,
+  newVote,
 };
