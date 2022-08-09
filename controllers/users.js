dff0096d3a1e1cfafa3587aaf8136398ab277d79
Elsa CrCa <98592187+elcrca@users.noreply.github.com>
2022-08-09 19:20:29 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const Joi = require('@hapi/joi');
 const { generateError } = require('../helpers');
 const { createUser, getUserById, getUserByEmail } = require('../DDBB/usersdb');
 
