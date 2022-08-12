708301c671882d2666a01c277449cc7277895c52
Your Name <you@example.com>
2022-08-12 13:16:16 +0200

    update votes

--- DDBB/votesdb.js
+++ DDBB/votesdb.js
@@ -1,20 +1,35 @@
 const { getConnection } = require('../DDBB/db');
+const { generateError } = require('../helpers');
 
-const newVote = async (vote, user_id, comment_id) => {
+const newVote = async (user_id, comment_id, vote) => {
   let connection;
 
   try {
     connection = await getConnection();
+    // imprescindible comprobar que el usuario no haya votado ya ese comentario.
+    const [formervote] = await connection.query(
+      `
+      SELECT id FROM votes WHERE user_id = ? AND comment_id = ?
+      `,
+      [user_id, comment_id]
+    );
+
+    if (formervote.length > 0) {
+      throw generateError(
+        `el usuario ${user_id} ya ha votado el comentario ${comment_id}`,
+        409
+      );
+    }
 
     const [result] = await connection.query(
       `
         INSERT INTO votes (user_id, vote,comment_id)
-        VALUES (? ? ?)
+        VALUES (?, ?, ?)
         `,
       [user_id, vote, comment_id]
     );
 
-    return result.insertVote;
+    return result.insertId;
   } finally {
     if (connection) connection.release();
   }
--- controllers/votes.js
+++ controllers/votes.js
@@ -3,17 +3,20 @@ const { generateError } = require('../helpers');
 
 const newVoteController = async (req, res, next) => {
   try {
-    const { vote } = req.body;
+    const { vote, comment_id } = req.body;
 
-    if (!vote) {
-      throw generateError('Any vote has been genated', 400);
+    if (!vote || !comment_id) {
+      throw generateError(
+        ' los campos vote y comment_id son obligatorios',
+        400
+      );
     }
 
-    const id = await newVote(req.user_id);
+    const id = await newVote(req.userId, comment_id, vote);
 
     res.send({
       status: 'ok',
-      message: `Vote with id: ${id} succesfully created`,
+      message: `Vote con id: ${id} creado correctamente`,
     });
   } catch (error) {
     next(error);
